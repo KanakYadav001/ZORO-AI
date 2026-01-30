1fd0656ca8dfb4e29aa36bd7c6a343f37a3e3dfe
Kanak Yadav <yadavkanak109@gmail.com>
2026-01-30 15:33:47 +0530

    fix: format code for consistency and readability in pinecone.service.js

--- backend/src/services/pinecone.service.js
+++ backend/src/services/pinecone.service.js
@@ -1,33 +1,30 @@
 // Import the Pinecone library
-const  { Pinecone }  = require('@pinecone-database/pinecone')
+const { Pinecone } = require("@pinecone-database/pinecone");
 
 // Initialize a Pinecone client with your API key
 const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY });
 
 // Create a dense index with integrated embedding
-const indexName = pc.Index('chat-gpt-project');
+const indexName = pc.Index("chat-gpt-project");
 
-
-
-async function upsertVectors({Id , vectors,metadata}) {
-    await indexName.upsert([{
-       id : Id,
-       values : vectors,
-       metadata : metadata,
-       
-    }]);
+async function upsertVectors({ Id, vectors, metadata }) {
+  await indexName.upsert([
+    {
+      id: Id,
+      values: vectors,
+      metadata: metadata,
+    },
+  ]);
 }
 
-
-async function queryVectors({vectors,limit=5,metadata}) {
-    const queryResponse = await indexName.query({
-            vector: vectors,
-            topK: limit,
-            filter : metadata ? {metadata} : undefined,
-            includeMetadata: true,
-    });
-    return queryResponse.matches;
+async function queryVectors({ vectors, limit = 5, metadata }) {
+  const queryResponse = await indexName.query({
+    vector: vectors,
+    topK: limit,
+    filter: metadata ? metadata : undefined,
+    includeMetadata: true,
+  });
+  return queryResponse.matches;
 }
 
-
-module.exports = { upsertVectors,queryVectors };
+module.exports = { upsertVectors, queryVectors };
